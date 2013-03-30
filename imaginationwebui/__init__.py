888dec42a8e43f6d1d14183eb3e7e299c72228f2
Bruno Binet <bruno.binet@gmail.com>
2013-03-30 12:35:34 +0100

    configure use of acidfs repository

--- imaginationwebui/__init__.py
+++ imaginationwebui/__init__.py
@@ -1,11 +1,25 @@
+import transaction
+
 from pyramid.config import Configurator
+from pyramid.events import NewRequest
+from pyramid.events import subscriber
+from acidfs import AcidFS
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+
+    config.registry.afs = AcidFS(settings['acidfs.repository'])
+
+    config.include('pyramid_tm')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
+
+
+@subscriber(NewRequest)
+def set_user(event):
+    transaction.get().setUser("Anonymous")
