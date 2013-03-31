b87756fb80a515ca6db24849434158b4086f28b6
Bruno Binet <bruno.binet@gmail.com>
2013-03-31 01:33:41 +0100

    add list view which will return list of slides data

--- imaginationwebui/__init__.py
+++ imaginationwebui/__init__.py
@@ -19,6 +19,7 @@ def main(global_config, **settings):
 
     config.include('pyramid_tm')
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('list', '/')
     config.add_route('show', '/')
     config.add_route('update', '/update')
     config.scan()
--- imaginationwebui/views.py
+++ imaginationwebui/views.py
@@ -1,6 +1,20 @@
 from pyramid.view import view_config
 
 
+@view_config(route_name='list', renderer='json')
+def list(request):
+    slides = []
+    imgconfig = request.registry.imgconfig
+    nb_slides = imgconfig.getint('slideshow settings', 'number of slides')
+    for i in range(nb_slides):
+        slide = 'slide %d' % (i + 1)
+        slides.append({
+            'url': imgconfig.get(slide, 'filename'),
+            'text': imgconfig.get(slide, 'text'),
+            })
+    return slides
+
+
 @view_config(route_name='show', renderer='string')
 def show(request):
     return 'show'
