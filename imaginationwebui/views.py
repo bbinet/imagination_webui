06c1c6c199777ab76335d7a4ff22d48cb9a00c5e
Bruno Binet <bruno.binet@gmail.com>
2013-04-02 02:41:16 +0200

    improve flickrimport view
    do not erase by default (request param configurable)
    setid id is now configurable from request params

--- imaginationwebui/views.py
+++ imaginationwebui/views.py
@@ -1,8 +1,10 @@
 import json
 import urllib2
+from operator import itemgetter
 
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
+from pyramid.settings import asbool
 
 
 @view_config(route_name='index')
@@ -37,18 +39,27 @@ def export(request):
 
 @view_config(route_name='flickrimport', renderer='json')
 def flickrimport(request):
+    setid = request.params.get('setid', '72157633137000735')
+    slides = {} if asbool(request.params.get('erase', False)) \
+                else request.registry.slides
     url = 'http://api.flickr.com/services/rest/' \
             '?method=flickr.photosets.getPhotos' \
             '&api_key=268c0d669b7b50a951bd0d6bb9ab2669' \
-            '&photoset_id=72157633137000735&format=json' \
-            '&nojsoncallback=1&extras=url_n,url_o,url_l'
+            '&photoset_id=%s&format=json&nojsoncallback=1' \
+            '&extras=url_n,url_o,url_l' % setid
+            #'&api_key=2a2ce06c15780ebeb0b706650fc890b2'
     data = json.load(urllib2.urlopen(url))
-    slides = {}
-    for i, photo in enumerate(data['photoset']['photo']):
-        slides[str(i)] = {
+    photos = data['photoset']['photo']
+    urls = map(itemgetter('url'), slides.values())
+    count = len(slides)
+    for photo in photos:
+        if photo['url_n'] in urls:
+            continue
+        slides[str(count)] = {
                 'url': photo['url_n'],
                 'text': photo['title'],
-                'position': i,
+                'position': count,
                 }
+        count += 1
     request.registry.slides = slides
     return slides
