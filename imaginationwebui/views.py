299cdf7cead68f18818107b6177f9b17163140b8
Bruno Binet <bruno.binet@gmail.com>
2013-04-05 21:54:22 +0200

    add support for display different sizes oh thumbs
    using query string ?size=x  (with: 0 <= x <= 4)

--- imaginationwebui/views.py
+++ imaginationwebui/views.py
@@ -14,7 +14,14 @@ def index(request):
 
 @view_config(route_name='list', renderer='json')
 def list(request):
-    return request.registry.slides.get()
+    maxsize = int(request.params.get('size', 3))
+    slides = request.registry.slides.get()
+    for slide in slides.itervalues():
+        thumb_urls = slide['thumb_urls']
+        del slide['thumb_urls']
+        size = min(maxsize, len(thumb_urls) - 1)
+        slide['thumb_url'] = thumb_urls[-(size + 1)]
+    return slides
 
 
 @view_config(route_name='reorder', renderer='json')
@@ -49,17 +56,17 @@ def flickrimport(request):
 
     # urltype > (suffix from http://www.flickr.com/services/api/misc.urls.html)
     thumb_urltypes = [
+            'url_m',  # (suffix -) medium, 500 on longest side
             'url_n',  # (suffix n) small, 320 on longest side
             'url_s',  # (suffix m) small, 240 on longest side
             'url_t',  # (suffix t) thumbnail, 100 on longest side
-           #'url_sq', # (suffix s) small square 75x75
+            'url_sq', # (suffix s) small square 75x75
             ]
     urltypes = [
             'url_o',  # (suffix o) original image
             'url_l',  # (suffix b) large, 1024 on longest side
             'url_c',  # (suffix c) medium 800, 800 on longest side
             'url_z',  # (suffix z) medium 640, 640 on longest side
-            'url_m',  # (suffix -) medium, 500 on longest side
             ] + thumb_urltypes
 
     url = 'http://api.flickr.com/services/rest/' \
@@ -70,33 +77,31 @@ def flickrimport(request):
     data = json.load(urllib2.urlopen(url))
 
     photos = data['photoset']['photo']
-    thumb_urls = map(itemgetter('thumb_url'), slides.values())
+    urls = map(itemgetter('url'), slides.values())
     count = len(slides)
 
     def _find_urls(photo):
-        """This will return the urls (thumb_url, url) for the biggest photo
+        """This will return the urls (thumb_urls, url) for the biggest photo
         available or None if the photo already exists in the datastore"""
-        thumb_url = None
         url = None
-        for t in thumb_urltypes:
-            if t in photo:
-                if photo[t] in thumb_urls:
-                    return None, None
-                thumb_url = photo[t]
-                break
         for t in urltypes:
             if t in photo:
+                if photo[t] in urls:
+                    return None, None
                 url = photo[t]
                 break
-        return thumb_url, url
+        thumb_urls = []
+        for t in thumb_urltypes:
+            if t in photo:
+                thumb_urls.append(photo[t])
+        return thumb_urls, url
 
     for photo in photos:
-        thumb_url, url = _find_urls(photo)
-        if thumb_url is None:
+        thumb_urls, url = _find_urls(photo)
+        if url is None:
             continue
-
         slides[str(count)] = {
-                'thumb_url': thumb_url,
+                'thumb_urls': thumb_urls,
                 'url': url,
                 'text': photo['title'],
                 'position': count,
